4473ef6e5076a2e23f67b321493619fa90a19a58
Tobias Becerra <tobiasbecerra2004@gmail.com>
2024-06-08 12:00:59 -0300

    Actualizacion completa, AVL, AB y recorridos agregados

--- main.cpp
+++ main.cpp
@@ -6,30 +6,64 @@ int main() {
 	setlocale(LC_ALL, "");
 	Lista<int> lista;
 	ArbolBinarioBusqueda<int> abb;
+	ArbolBinario<int> ab;
+	ArbolAVL<int> avl;
 	int opc = 0;
 	cout << "\tBIENVENIDO AL GENERADOR DE ARBOLES BLAS PASCAL" << endl;
 	lista.imprimirLista();
 
-	while (opc != 5) {	
+	while (opc != 6) {
 
-		cout << "\n\t1. Generar árbol binario."  << endl;
+		system("cls");
+
+		cout << "\n\t1. Generar árbol binario." << endl;
 		cout << "\n\t2. Generar árbol binario de búsqueda." << endl;
 		cout << "\n\t3. Generar AVL." << endl;
 		cout << "\n\t4. Generar árbol rojo y negro." << endl;
-		cout << "\n\t5. Salir del generador." << endl;
+		cout << "\n\t5. Mostrar recorridos Pre, In y Post ORDER del árbol ingresado." << endl;
+		cout << "\n\t6. Salir del generador." << endl;
 		cout << "\nIngrese opcion: ";
 		cin >> opc;
 
 		switch (opc) {
 		case 1:
+			system("cls");
+			ab.construirArbolDesdeLista(lista);
+			cout << "\nRepresentación gráfica del árbol binario:\n";
+			ab.imprimirGrafico();
+
+			cin.ignore();
+			cin.get();
 			break;
 		case 2:
+			system("cls");
 			abb.construirArbolDesdeLista(lista);
+			cout << "\nRepresentación gráfica del árbol binario:\n";
 			abb.imprimirGrafico();
+			cin.ignore();
+			cin.get();
 			break;
 		case 3:
+			system("cls");
+			avl.construirArbolDesdeLista(lista);
+			cout << "\nRepresentación gráfica del árbol binario:\n";
+			avl.imprimirGrafico();
+			cin.ignore();
+			cin.get();
 			break;
 		case 4:
+			system("cls");
+
+
+			break;
+		case 5:
+			system("cls");
+			lista.recorrerPreorden();
+			lista.recorrerInorden();
+			lista.recorrerPostorden();
+			cin.ignore();
+			cin.get();
+
 			break;
 		}
 	}
--- trees.h
+++ trees.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <queue>
 using namespace std;
 
 //CLASE NODO
@@ -17,7 +18,7 @@ public:
 template <typename T>
 class Lista {
 public:
-    Nodo<T>* cabeza;
+	Nodo<T>* cabeza;
 	Lista() :cabeza(nullptr) {};
 
 	//FUNCION AGREGAR ELEMENTOS A LA LISTA
@@ -86,6 +87,51 @@ public:
 			}
 		} while (!listaCorrecta);
 	}
+
+	// Recorrido Preorden
+	void preorden(Nodo<T>* nodo) {
+		if (nodo != nullptr) {
+			cout << nodo->dato << " ";
+			preorden(nodo->siguiente);
+		}
+	}
+
+	// Recorrido Inorden
+	void inorden(Nodo<T>* nodo) {
+		if (nodo != nullptr) {
+			inorden(nodo->siguiente);
+			cout << nodo->dato << " ";
+		}
+	}
+
+	// Recorrido Postorden
+	void postorden(Nodo<T>* nodo) {
+		if (nodo != nullptr) {
+			postorden(nodo->siguiente);
+			cout << nodo->dato << " ";
+		}
+	}
+
+	// Función para iniciar el recorrido preorden
+	void recorrerPreorden() {
+		cout << "\nRecorrido PRE-ORDER --> ";
+		preorden(cabeza);
+		cout << endl;
+	}
+
+	// Función para iniciar el recorrido inorden
+	void recorrerInorden() {
+		cout << "\nRecorrido IN-ORDER --> ";
+		inorden(cabeza);
+		cout << endl;
+	}
+
+	// Función para iniciar el recorrido postorden
+	void recorrerPostorden() {
+		cout << "\nRecorrido POST-ORDER --> ";
+		postorden(cabeza);
+		cout << endl;
+	}
 };
 
 template <typename T>
@@ -142,6 +188,203 @@ private:
 		if (nodo != nullptr) {
 			imprimirGraficoRecursivo(nodo->derecha, nivel + 1);
 			std::cout << std::string(4 * nivel, ' ') << nodo->dato << std::endl;
+			imprimirGraficoRecursivo(nodo->izquierda, nivel + 1);
+		}
+	}
+};
+
+template <typename T>
+class ArbolAVL {
+private:
+	struct NodoAVL {
+		T dato;
+		NodoAVL* izquierda;
+		NodoAVL* derecha;
+		int altura;
+
+		NodoAVL(T dato) : dato(dato), izquierda(nullptr), derecha(nullptr), altura(1) {}
+	};
+
+	NodoAVL* raiz;
+
+
+	int obtenerAltura(NodoAVL* nodo) {
+		if (nodo == nullptr)
+			return 0;
+		return nodo->altura;
+	}
+
+
+
+	int obtenerBalance(NodoAVL* nodo) {
+		if (nodo == nullptr)
+			return 0;
+
+		return obtenerAltura(nodo->izquierda) - obtenerAltura(nodo->derecha);
+	}
+
+
+	NodoAVL* rotacionDerecha(NodoAVL* y) {
+		NodoAVL* x = y->izquierda;
+		NodoAVL* T2 = x->derecha;
+
+		x->derecha = y;
+		y->izquierda = T2;
+
+		y->altura = max(obtenerAltura(y->izquierda), obtenerAltura(y->derecha)) + 1;
+		x->altura = max(obtenerAltura(x->izquierda), obtenerAltura(x->derecha)) + 1;
+
+		return x;
+	}
+
+	NodoAVL* rotacionIzquierda(NodoAVL* x) {
+		NodoAVL* y = x->derecha;
+		NodoAVL* T2 = y->izquierda;
+
+		y->izquierda = x;
+		x->derecha = T2;
+
+		x->altura = max(obtenerAltura(x->izquierda), obtenerAltura(x->derecha)) + 1;
+		y->altura = max(obtenerAltura(y->izquierda), obtenerAltura(y->derecha)) + 1;
+
+		return y;
+	}
+
+
+	NodoAVL* insertarRecursivo(NodoAVL* nodo, T dato) {
+		if (nodo == nullptr) {
+			return new NodoAVL(dato);
+		}
+		if (dato < nodo->dato) {
+			nodo->izquierda = insertarRecursivo(nodo->izquierda, dato);
+		}
+		else if (dato > nodo->dato) {
+			nodo->derecha = insertarRecursivo(nodo->derecha, dato);
+		}
+		else {
+			return nodo;
+		}
+
+		nodo->altura = 1 + max(obtenerAltura(nodo->izquierda), obtenerAltura(nodo->derecha));
+
+		int balance = obtenerBalance(nodo);
+
+
+		if (balance > 1 && dato < nodo->izquierda->dato) {
+			return rotacionDerecha(nodo);
+		}
+
+		// Caso Rotación Derecha
+		if (balance < -1 && dato > nodo->derecha->dato) {
+			return rotacionIzquierda(nodo);
+		}
+
+		return nodo;
+	}
+
+
+	void imprimirGraficoRecursivo(NodoAVL* nodo, int nivel) {
+		if (nodo != nullptr) {
+			imprimirGraficoRecursivo(nodo->derecha, nivel + 1);
+			std::cout << std::string(4 * nivel, ' ') << nodo->dato << std::endl;
+			imprimirGraficoRecursivo(nodo->izquierda, nivel + 1);
+		}
+	}
+
+public:
+	ArbolAVL() : raiz(nullptr) {}
+
+
+	void construirArbolDesdeLista(Lista<T>& lista) {
+		Nodo<T>* temp = lista.cabeza;
+		while (temp != nullptr) {
+			insertar(temp->dato);
+			temp = temp->siguiente;
+		}
+	}
+
+	void insertar(T dato) {
+		raiz = insertarRecursivo(raiz, dato);
+	}
+
+
+	void imprimirGrafico() {
+		imprimirGraficoRecursivo(raiz, 0);
+	}
+};
+
+// CLASE ARBOL BINARIO
+template <typename T>
+class ArbolBinario {
+private:
+	struct NodoAB {
+		T dato;
+		NodoAB* izquierda;
+		NodoAB* derecha;
+		NodoAB(T dato) : dato(dato), izquierda(nullptr), derecha(nullptr) {}
+	};
+
+	NodoAB* raiz;
+
+	// Función auxiliar para insertar un elemento en el árbol de forma secuencial usando una cola
+	NodoAB* insertar(NodoAB* nodo, T dato) {
+		if (nodo == nullptr)
+			return new NodoAB(dato);
+
+		queue<NodoAB*> q;
+		q.push(nodo);
+
+		while (!q.empty()) {
+			NodoAB* actual = q.front();
+			q.pop();
+
+			if (actual->izquierda == nullptr) {
+				actual->izquierda = new NodoAB(dato);
+				return nodo;
+			}
+			else {
+				q.push(actual->izquierda);
+			}
+
+			if (actual->derecha == nullptr) {
+				actual->derecha = new NodoAB(dato);
+				return nodo;
+			}
+			else {
+				q.push(actual->derecha);
+			}
+		}
+		return nodo;
+	}
+
+public:
+	ArbolBinario() : raiz(nullptr) {}
+
+	// Función para construir el árbol a partir de la lista
+	void construirArbolDesdeLista(Lista<T>& lista) {
+		Nodo<T>* temp = lista.cabeza;
+		while (temp != nullptr) {
+			insertar(temp->dato);
+			temp = temp->siguiente;
+		}
+	}
+
+	// Función para insertar un elemento en el árbol
+	void insertar(T dato) {
+		raiz = insertar(raiz, dato);
+	}
+
+	// Función para imprimir el árbol de manera gráfica
+	void imprimirGrafico() {
+		imprimirGraficoRecursivo(raiz, 0);
+	}
+
+private:
+	// Función recursiva para imprimir el árbol de manera gráfica
+	void imprimirGraficoRecursivo(NodoAB* nodo, int nivel) {
+		if (nodo != nullptr) {
+			imprimirGraficoRecursivo(nodo->derecha, nivel + 1);
+			cout << string(4 * nivel, ' ') << nodo->dato << endl;
 			imprimirGraficoRecursivo(nodo->izquierda, nivel + 1);
 		}
 	}
